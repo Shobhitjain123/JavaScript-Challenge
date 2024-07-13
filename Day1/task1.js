d95af2473f3ffee699e09f111ef17f04cea08bc8
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-07-13 11:46:25 +0530

    Day 1 challenge Complete

---
+++ Day1/task1.js
@@ -0,0 +1,42 @@
+// Task 1 
+
+var var1 = 10; //Creating new variable with var keyword
+console.log(var1); //logging the Variable to console
+
+// Task2
+
+let var2 = "Hello JS"; // Creating new variable with let keyword
+console.log(var2); //logging the variable to console
+
+// Task3
+
+const var3 = true; // Creating new variable with const keyword
+console.log(var3); // logging the variabe to console
+
+// Task4
+
+const vartype1 = 11;
+const vartype2 = "steing";
+const vartype3 = false;
+const vartype4 = ['hello js', 23, true];
+const vartype5 = {username: "JS"};
+
+console.log(typeof vartype1)
+console.log(typeof vartype2)
+console.log(typeof vartype3)
+console.log(typeof vartype4)
+console.log(typeof vartype5)
+
+
+// Task 5
+
+let assignVar = 40
+console.log(assignVar);
+assignVar = 100
+console.log(assignVar);
+
+// Task 6
+
+const reassignVar = 25;
+reassignVar = 40;
+console.log(reassignVar);
