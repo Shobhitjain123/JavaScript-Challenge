8ec1c8c591488fbb8ecd20a571f51a479b36f914
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-07-15 21:25:08 +0530

    Day 3 Challenge done

---
+++ Day3/code.js
@@ -0,0 +1,141 @@
+// Task1
+
+// const num = prompt('Enter number');
+
+//  if(num == 0){
+//     alert('Number equals to 0');    
+//  }else if(num > 0){
+//     alert("Number is Positive");
+//  }else{
+//     alert('Number is negative');
+//  }
+
+ //Task2
+
+// const age = prompt("PLease enter your age");
+
+// if(age >= 18){
+//    alert("You are eligible to vote");
+// }else{
+//    alert("OOPS!! You are not eligible")
+// }
+ 
+
+// Task3
+
+// const num1 = prompt("Enter number 1")
+// const num2 = prompt("Enter number 2")
+// const num3 = prompt("Enter number 3")
+
+// if(num1 === num2){
+//    alert(`Num1 ${num1} and Num2 ${num2} are equal`);
+// }else if(num1 === num3) {
+//    alert(`Num1 ${num1} and Num3 ${num3} are equal`);
+// }else if(num2 === num3){
+//    alert(`Num2 ${num2} and Num3 ${num3} are equal`);
+// }else{
+//    if(num1 > num2){
+//       if(num1 > num3){
+//          alert(`Num1 ${num1} is greatest`);
+//       }else{
+//          alert(`Num3 ${num3} is greatest`);
+//       }
+//    }
+   
+//    else{
+//       if(num2 < num3){
+//          alert(`Num3 ${num3} is greatest`);
+//       }else{
+//          alert(`Num2 ${num2} is greatest`);
+//       }
+//    }
+// }
+
+// Task4
+
+// const num = Number(prompt("Enter number of the Day"))
+
+// switch(num) {
+//    case 1:
+//       alert("Today is Monday");
+//       break;
+//    case 2:
+//       alert("Today is Tuesday");
+//       break;
+//    case 3:
+//       alert("Today is Wednesday");
+//       break;
+//    case 4:
+//       alert("Today is Thursday");
+//       break;
+//    case 5: 
+//       alert("Today is Friday");
+//       break;
+//    case 6:
+//       alert("Today is satirday");
+//       break;
+//    case 7:
+//       alert("Today is Sunday");
+//       break;
+//    default:
+//       alert("Please enter valid number between 1-7");
+//       break;
+// }
+
+// Task5
+
+// const score = Number(prompt("Enter your score"))
+
+// switch(true) {
+//    case (score > 85):
+//       alert("You scored A Grade");
+//       break;
+//    case (score > 70 && score <= 85):
+//       alert("You scored B Grade");
+//       break;
+//    case (score > 70 && score <= 85):
+//       alert("Today is Wednesday");
+//       break;
+//    case 4:
+//       alert("Today is Thursday");
+//       break;
+//    case 5: 
+//       alert("Today is Friday");
+//       break;
+//    case 6:
+//       alert("Today is satirday");
+//       break;
+//    default:
+//       alert("Please enter valid number between 1-7");
+//       break;
+// }
+
+// Task6
+
+// const num = Math.floor((Math.random()* 100) + 1);
+//  console.log(num);
+// num % 2 == 0 ? console.log("Even Number") : console.log("Odd Number");
+
+// Task7
+
+// const num = Math.floor((Math.random()*4000) + 1000);
+// const num = 2024
+// console.log(num);
+// if(num % 4 == 0) {
+//    if(num % 100 == 0) {
+//       if(num % 400 == 0){
+//          console.log("It's a leap year");
+//       }
+//       else{
+//          console.log("It's not a leap year");
+//       }
+//    }
+//    else{
+//       console.log("It's a leap year");
+//    }
+// }
+// else{
+//    console.log("It's not a leap year");
+// }
+
+
