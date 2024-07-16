b5bec06d7a7467366bc9aaee8e2c579cc7139009
Shobhit Jain <42944341+Shobhitjain123@users.noreply.github.com>
2024-07-16 12:32:14 +0530

    Day 4 Challenge

---
+++ Day4/code.js
@@ -0,0 +1,69 @@
+// Task1
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index);
+    
+// }
+
+// Task2
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(`5 * ${index} = ${5*index}`);
+// }
+
+// Task3
+
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     sum = sum + num;
+//     num++;
+// }
+// console.log(sum);
+
+// Task4
+
+// let num = 10;
+// while(num >= 1){
+//     console.log(num);
+//     num--;
+// }
+
+// Task5
+
+// let num = 1;
+// do{
+//     console.log(num);
+//     num++;
+// }while(num <= 5);
+
+// Task6
+
+// let num = 6;
+// let factorial = 1;
+// do{
+//     factorial = factorial * num
+//     num--;
+// }while(num >= 1);
+// console.log(factorial);
+
+// Task7
+
+// for (let i = 1; i <= 5; i++) {
+//     let pattern = "* ";
+//     console.log(pattern.repeat(i));
+// }
+
+// Task8
+
+// for (let index = 1; index <= 10; index++) {
+//     if(index == 5) continue;
+//     console.log(index);
+// }
+
+// Task9
+
+// for (let index = 1; index <= 10; index++) {
+//     if(index == 7) break;
+//     console.log(index);
+// }
